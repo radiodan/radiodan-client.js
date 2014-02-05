08d6cc2c8b760968e1e2decf80b47f0ab1092d64
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-02-05 11:48:02 +0000

    Allow caller to set custom log level

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var RadiodanClient  = require('./lib/radiodan-client'),
-    MessagingClient = require('./lib/messaging-client');
+    MessagingClient = require('./lib/messaging-client'),
+    logger = require('./lib/utils').logger;
 
 module.exports.create = function (opts) {
   opts = opts || {};
 
   var client = MessagingClient.create(opts);
   return RadiodanClient.create(client);
+};
+
+module.exports.setLogLevel = function (level) {
+  logger.setLevel(level);
 };
