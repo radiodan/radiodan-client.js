514ee9ac3aad1876e0cbf005b55a5e136a0ddae1
Dan Nuttall <pixelblend@gmail.com>
2014-02-24 15:25:46 +0000

    Add player object to client lib

--- static/client.js
+++ static/client.js
@@ -1,3 +1,23 @@
-var hello = 'THIS IS IT';
+(function(){
+  var actions = {{actions}};
 
-alert(hello);
+  var player = {create: function(id){
+    var instance = {};
+    instance.id = 'radio.' + id;
+
+    Object.keys(actions).forEach(function(key, index){
+      var action = actions[key];
+      instance[key] = function (params) {
+        return sendCommandForAction(action, params);
+      };
+    });
+
+    function sendCommandForAction (action, params) {
+      console.log('sendCommandForAction', action, params);
+    }
+
+    return instance;
+  }};
+
+  window.radiodan = {player: player};
+})()
