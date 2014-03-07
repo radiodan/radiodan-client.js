6ee5c2e74c4c74ef81391f1e844d8b230debdffb
Dan Nuttall <pixelblend@gmail.com>
2014-03-07 17:06:51 +0000

    Move browserify and ugilify to grunt task, serve static file to clients

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      'public/client.js': ['static/client.js']
+    },
+    uglify : {
+      js: {
+        files: {
+          'public/client.min.js' : [ 'public/client.js' ]
+        }
+      }
+    },
+    watch: {
+      files: [ "static/**/*.js"],
+      tasks: [ 'default' ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', [ 'browserify', 'uglify:js' ]);
+}
