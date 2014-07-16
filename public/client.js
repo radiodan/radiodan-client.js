99c0737dd33e032cdd3e2a8d6df068d31c1d4834
Dan Nuttall <pixelblend@gmail.com>
2014-07-16 16:09:55 +0100

    Update static library

--- public/client.js
+++ public/client.js
@@ -2292,7 +2292,7 @@ var actions = require('./actions/audio.json'),
     commandAndEvents = require('./command-and-events');
 
 module.exports = function (url) {
-  var create = commandAndEvents.create(url, 'player', actions);
+  var create = commandAndEvents.create(url, 'audio', actions);
   return { create: create };
 };
 
