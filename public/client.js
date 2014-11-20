bd4a93057d2dfe6ceffd6706aeb58fa855f92b5b
Dan Nuttall <pixelblend@gmail.com>
2014-11-20 17:12:39 +0000

    Update generated client js

--- public/client.js
+++ public/client.js
@@ -683,22 +683,6 @@ if (typeof ReturnValue !== "undefined") {
     };
 }
 
-// Until V8 3.19 / Chromium 29 is released, SpiderMonkey is the only
-// engine that has a deployed base of browsers that support generators.
-// However, SM's generators use the Python-inspired semantics of
-// outdated ES6 drafts.  We would like to support ES6, but we'd also
-// like to make it possible to use generators in deployed browsers, so
-// we also support Python-style generators.  At some point we can remove
-// this block.
-var hasES6Generators;
-try {
-    /* jshint evil: true, nonew: false */
-    new Function("(function* (){ yield 1; })");
-    hasES6Generators = true;
-} catch (e) {
-    hasES6Generators = false;
-}
-
 // long stack traces
 
 var STACK_JUMP_SEPARATOR = "From previous event:";
@@ -999,6 +983,7 @@ defer.prototype.makeNodeResolver = function () {
  * @returns a promise that may be resolved with the given resolve and reject
  * functions, or rejected by a thrown exception in resolver
  */
+Q.Promise = promise; // ES6
 Q.promise = promise;
 function promise(resolver) {
     if (typeof resolver !== "function") {
@@ -1012,6 +997,11 @@ function promise(resolver) {
     }
     return deferred.promise;
 }
+
+promise.race = race; // ES6
+promise.all = all; // ES6
+promise.reject = reject; // ES6
+promise.resolve = Q; // ES6
 
 // XXX experimental.  This method is a way to denote that a local value is
 // serializable and should be immediately dispatched to a remote upon request,
@@ -1337,42 +1327,14 @@ Promise.prototype.isRejected = function () {
 // shimmed environments, this would naturally be a `Set`.
 var unhandledReasons = [];
 var unhandledRejections = [];
-var unhandledReasonsDisplayed = false;
 var trackUnhandledRejections = true;
-function displayUnhandledReasons() {
-    if (
-        !unhandledReasonsDisplayed &&
-        typeof window !== "undefined" &&
-        window.console
-    ) {
-        console.warn("[Q] Unhandled rejection reasons (should be empty):",
-                     unhandledReasons);
-    }
-
-    unhandledReasonsDisplayed = true;
-}
-
-function logUnhandledReasons() {
-    for (var i = 0; i < unhandledReasons.length; i++) {
-        var reason = unhandledReasons[i];
-        console.warn("Unhandled rejection reason:", reason);
-    }
-}
 
 function resetUnhandledRejections() {
     unhandledReasons.length = 0;
     unhandledRejections.length = 0;
-    unhandledReasonsDisplayed = false;
 
     if (!trackUnhandledRejections) {
         trackUnhandledRejections = true;
-
-        // Show unhandled rejection reasons if Node exits without handling an
-        // outstanding rejection.  (Note that Browserify presently produces a
-        // `process` global without the `EventEmitter` `on` method.)
-        if (typeof process !== "undefined" && process.on) {
-            process.on("exit", logUnhandledReasons);
-        }
     }
 }
 
@@ -1387,7 +1349,6 @@ function trackRejection(promise, reason) {
     } else {
         unhandledReasons.push("(no stack) " + reason);
     }
-    displayUnhandledReasons();
 }
 
 function untrackRejection(promise) {
@@ -1411,9 +1372,6 @@ Q.getUnhandledReasons = function () {
 
 Q.stopUnhandledRejectionTracking = function () {
     resetUnhandledRejections();
-    if (typeof process !== "undefined" && process.on) {
-        process.removeListener("exit", logUnhandledReasons);
-    }
     trackUnhandledRejections = false;
 };
 
@@ -1577,7 +1535,17 @@ function async(makeGenerator) {
         // when verb is "throw", arg is an exception
         function continuer(verb, arg) {
             var result;
-            if (hasES6Generators) {
+
+            // Until V8 3.19 / Chromium 29 is released, SpiderMonkey is the only
+            // engine that has a deployed base of browsers that support generators.
+            // However, SM's generators use the Python-inspired semantics of
+            // outdated ES6 drafts.  We would like to support ES6, but we'd also
+            // like to make it possible to use generators in deployed browsers, so
+            // we also support Python-style generators.  At some point we can remove
+            // this block.
+
+            if (typeof StopIteration === "undefined") {
+                // ES6 Generators
                 try {
                     result = generator[verb](arg);
                 } catch (exception) {
@@ -1589,6 +1557,7 @@ function async(makeGenerator) {
                     return when(result.value, callback, errback);
                 }
             } else {
+                // SpiderMonkey Generators
                 // FIXME: Remove this case when SM does ES6 generators.
                 try {
                     result = generator[verb](arg);
@@ -2309,6 +2278,7 @@ module.exports={
 },{}],6:[function(require,module,exports){
 module.exports={
   "add": "playlist.add",
+  "load": "playlist.load",
   "clear": "playlist.clear",
   "next": "player.next",
   "pause": "player.pause",
@@ -2392,33 +2362,36 @@ function create(url, namespace, actions) {
           function () {
             return sendCommandForAction(action, options);
           }
-          );
+        );
       };
     });
 
     function sendCommandForAction (action, options) {
       console.log('sendCommandForAction', action, options);
-      return xhr(
-          url + '/radiodan/command/' + instance.path,
-          {action: action, options: options}
-          ).then(
-            function(response) {
-              if(response.error) {
-                console.warn('Command failed', response.error);
-                return Q.reject(response.error);
-              } else {
-                console.log('Command completed', response);
-                return Q.resolve(response.data);
-              }
+      var request = xhr(
+            url + '/radiodan/command/' + instance.path,
+            {action: action, options: options}
+          ),
+          parse = function(response) {
+            if(response.error) {
+              console.warn('Command failed', response.error);
+              return Q.reject(response.error);
+            } else {
+              console.log('Command completed', response);
+              return Q.resolve(response.data);
             }
-            );
+          };
+
+      return request.then(parse);
     }
 
     function subscribeToEvents(handler) {
       var deferred = Q.defer();
 
       if (!eventSource) {
-        eventSource = new EventSource(url + '/radiodan/stream/' + instance.path);
+        eventSource = new EventSource(
+          url + '/radiodan/stream/' + instance.path
+        );
       }
 
       eventSource.addEventListener('message', function (evt) {
